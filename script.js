31e0409f9b4fd1827cb392035b085e07c37e776a
Zach Wellens <zachwellens990@gmail.com>
2023-05-02 15:58:25 -0400

    Made the rows change color with respect to the time

--- script.js
+++ script.js
@@ -15,6 +15,38 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+  var timeSections = $(".time-block");
+  function updateTime() {
+    var currentHour = dayjs().format("H");
+    for (var i = 1; i < timeSections.length; i++) {
+      timeSections[i].classList.remove("past");     // These are the actual document elements 
+      timeSections[i].classList.remove("present");  //  not jquery elements, so you have to use
+      timeSections[i].classList.remove("future");   //  this notation
+      sectionTime = timeSections[i].id;
+      sectionTime = sectionTime.substring(5);
+      if (sectionTime < currentHour) {
+        timeSections[i].classList.add("past");
+      } else if (sectionTime > currentHour) {
+        timeSections[i].classList.add("future");
+      } else {
+        timeSections[i].classList.add("present");
+      }
+    }
+    // For some reason the hour-9 row wasn't working in the for loop, so I readdress it directly
+    //  the for loop now starts at i=1 so as to avoid redundancy
+    var firstRow = $("#hour-9");
+    firstRow.removeClass("past present future");
+    if (currentHour < 9) {
+      firstRow.addClass("future");
+    } else if (currentHour > 9) {
+      firstRow.addClass("past");
+    } else {
+      firstRow.addClass("present");
+    }
+  }
+  updateTime();
+  setRowColors = setInterval(updateTime, 1000)
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
@@ -23,9 +55,9 @@ $(function () {
   currentDay = $("#currentDay");
   currentTime = $("#currentTime");
   currentDay.text(dayjs().format("MMM D, YYYY"));
-  currentTime.text(dayjs().format("h:m:s"));
+  currentTime.text(dayjs().format("h:mm:s"));
   setDayAndTime = setInterval(function () {
     currentDay.text(dayjs().format("MMM D, YYYY"));
-    currentTime.text(dayjs().format("h:m:s"));
+    currentTime.text(dayjs().format("h:mm:s"));
   }, 1000)
 });
